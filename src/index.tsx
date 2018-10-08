259ecb565058360e356423995d1c835bd1e0637d
Your Name <you@example.com>
2018-10-08 17:20:28 +0800

    引入redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+// import {createStore} from 'redux';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-
+// tslint:disable-next-line:no-console
+console.log(React)
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
