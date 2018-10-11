854627c08d385027cedc239d4359e84184bdec84
Your Name <you@example.com>
2018-10-11 10:27:57 +0800

    全局引入antd样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import {connect} from 'react-redux';
 // import { DatePicker, LocaleProvider, message } from 'antd';
+import 'antd-mobile/dist/antd-mobile.css';
 import * as React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,11 +1,11 @@
-import { Button } from 'antd';
+import { Button } from 'antd-mobile';
+// import 'antd-mobile/lib/button/style/css';
 import * as React from 'react';
 import './index.css';
 class Home extends React.Component {
     public render() {
       return (
         <div className="Home">
-        asdsad  
             <Button>sada</Button>
         </div>
       );
